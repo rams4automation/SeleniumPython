f416f4802a93bb9c006f60fa6df833ca12250ce6
Ramesh <rams4automation@gmail.com>
2019-11-28 18:16:35 +0530

    Add Updated code

--- Python/RegExp.py
+++ Python/RegExp.py
@@ -1,9 +1,10 @@
 
-# match This method matches the regex pattern in the string with the optional flag. It returns true if a match is found in the string otherwise it returns false.
-# search	This method returns the match object if there is a match found in the string.
-# findall	It returns a list that contains all the matches of a pattern in the string.
-# split	Returns a list in which the string has been split in each match.
-# sub	Replace one or many matches in the string.
+# match -This method matches the regex pattern in the string with the optional flag. It returns true if a match is found
+#        in the string otherwise it returns false.
+# search	-This method returns the match object if there is a match found in the string.
+# findall	-It returns a list that contains all the matches of a pattern in the string.
+# split	-Returns a list in which the string has been split in each match.
+# sub	-Replace one or many matches in the string.
 
 import re
 
@@ -14,6 +15,14 @@ matches=re.findall("How",str)
 
 print(matches)
 
+str = "The rain in Spain"
+x = re.findall("ai", str)
+print(x)
+
+str = "The rain in Spain"
+x = re.findall("Portugal", str)
+print(x)
+
 
 # ************************************** Search **********************************
 
@@ -22,3 +31,41 @@ str1 = "How are you. How is everything"
 matches=re.search("How",str1)
 
 print(matches)
+
+str = "The rain in Spain"
+x = re.search("\s", str)
+print("The first white-space character is located in position:", x.start())
+
+str = "The rain in Spain"
+x = re.search("Portugal", str)
+print(x)
+
+# ******************************************************************************
+
+str = "How are you. How is everything"
+
+matches = re.search("How", str)
+
+print(matches.span())
+
+print(matches.group())
+
+print(matches.string)
+
+# ********************************** Split ****************************
+
+str="Twelve:12 Eighty nine:89."
+pa="[e]"
+result=re.split(pa, str)
+print(result)
+
+str = "The rain in Spain"
+x = re.split("\s", str)
+print(x)
+
+str = "The rain in Spain"
+x = re.split("\s", str, 1)
+print(x)
+
+
+
