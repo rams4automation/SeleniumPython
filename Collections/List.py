45d4b2e869d07db55f27e22379b4f6d2e0849675
Ramesh <rams4automation@gmail.com>
2019-11-29 18:57:26 +0530

    Add Updated code

---
+++ Collections/List.py
@@ -0,0 +1,30 @@
+
+li = ["apple", "banana", "cherry"]
+print(li)
+
+print(li[0])
+
+# Negative Indexing
+print(li[-1])
+
+# Range of Indexes
+list1 = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(list1[2:5])
+
+list2 = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(list2[:4])
+
+list3 = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(list3[2:])
+
+list4 = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(list4[-4:-1])
+
+
+#  Change Item Value
+
+Clist = ["apple", "banana", "cherry"]
+Clist[1] = "blackcurrant"
+print(Clist)
+
+
