595199810376a08c18a3e5b2d66ac864a6a03b99
Ramesh <rams4automation@gmail.com>
2019-12-06 18:52:03 +0530

    Add Updated code

--- SeleniumwithPython/MouseHover.py
+++ SeleniumwithPython/MouseHover.py
@@ -1,9 +1,9 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver import ActionChains
 import time
 
-driver=webdriver.Chrome(executable_path="D:\\chromedriver_win32\\chromedriver.exe")
+
+driver= webdriver.Chrome(executable_path="C:\\Drivers\\chromedriver.exe")
 
 driver.maximize_window()
 
@@ -11,21 +11,15 @@ driver.implicitly_wait(10)
 
 driver.get("https://demoqa.com/menu/")
 
-time.sleep(9)
+time.sleep(5)
 
 Music=driver.find_element_by_xpath("//*[@id='ui-id-9']")
 Rock=driver.find_element_by_xpath("//*[@id='ui-id-10']")
 Alternative=driver.find_element_by_xpath("//*[@id='ui-id-11']")
 
-time.sleep(9)
-
 action=ActionChains(driver)
 action.move_to_element(Music).move_to_element(Rock).move_to_element(Alternative).click().perform()
 
-time.sleep(9)
+time.sleep(5)
 
 driver.close()
-
-
-
-
